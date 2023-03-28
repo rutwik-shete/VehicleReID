5a651b0738118013097a54083a5078d455276a6a
Rutwik Shete <rutwik.shete@gmail.com>
2023-03-28 22:41:53 +0100

    Solved Error In MPS Support

--- src/utils/generaltools.py
+++ src/utils/generaltools.py
@@ -4,13 +4,13 @@ import random
 
 import numpy as np
 import torch
-import torch.mps as mps
 
 def set_random_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.has_mps:
+        import torch.mps as mps
         mps.manual_seed(seed)
     else:
         torch.cuda.manual_seed_all(seed)
