16a01507685891018e883a3b97e337282204d240
Rutwik Shete <rutwik.shete@gmail.com>
2023-03-29 23:19:51 +0100

    Changed Code To Append In Log File

--- src/utils/loggers.py
+++ src/utils/loggers.py
@@ -18,7 +18,7 @@ class Logger:
         self.file = None
         if fpath is not None:
             mkdir_if_missing(osp.dirname(fpath))
-            self.file = open(fpath, "w")
+            self.file = open(fpath, "a")
 
     def __del__(self):
         self.close()
